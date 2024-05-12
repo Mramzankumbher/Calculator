d9db7926a072e57244ce8addef2aa2b05df32a3b
Mramzankumbher <ra860379@gmail.com>
2024-05-12 13:33:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([
+    {
+        type: "number",
+        name: "first_number",
+        message: "please enter your first number"
+    },
+    {
+        type: "number",
+        name: "second_number",
+        message: "please enter your second number"
+    },
+    {
+        type: "list",
+        name: "operators",
+        message: "select operation number",
+        choices: ["addition", "substraction", "multiplication", "division"]
+    }
+]);
+if (ans.operators === "addition") {
+    console.log(ans.first_number + ans.second_number);
+}
+else if (ans.operators === "substraction") {
+    console.log(ans.first_number - ans.second_number);
+}
+else if (ans.operators === "multiplication") {
+    console.log(ans.first_number * ans.second_number);
+}
+else if (ans.operators === "division") {
+    console.log(ans.first_number / ans.second_number);
+}
